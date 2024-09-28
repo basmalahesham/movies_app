61dd52a1b8d812696ce6bf3035f5ba55c657fda0
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-29 00:14:17 +0300

    provide cubit and trigger in home_view.dart

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app/core/utils/service_locator.dart';
+import 'package:movies_app/features/home/data/repos/home_repo_implementation.dart';
+import 'package:movies_app/features/home/presentation/manager/latest_movies_cubit/latest_movies_cubit.dart';
+import 'package:movies_app/features/home/presentation/manager/popular_movies_cubit/popular_movies_cubit.dart';
+import 'package:movies_app/features/home/presentation/manager/tob_rated_movies_cubit/top_rated_movies_cubit.dart';
 import 'package:movies_app/features/home/presentation/views/widgets/home_view_body.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: HomeViewBody(),
+    return SafeArea(
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => PopularMoviesCubit(
+              getIt.get<HomeRepoImplementation>(),
+            )..fetchPopularMovie(),
+          ),
+          BlocProvider(
+            create: (context) => LatestMoviesCubit(
+              getIt.get<HomeRepoImplementation>(),
+            )..fetchLatestMovie(),
+          ),
+          BlocProvider(
+            create: (context) => TopRatedMoviesCubit(
+              getIt.get<HomeRepoImplementation>(),
+            )..fetchTopRatedMovie(),
+          ),
+        ],
+        child: const HomeViewBody(),
+      ),
     );
   }
 }
