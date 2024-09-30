fe7f7787a51509a2a096d51079f91936df02cdf1
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-30 23:16:50 +0300

    integrate SearchCubit with search_list_view.dart and display data in search_list_view_item.dart

--- lib/features/search/pesentation/views/widgets/search_list_view.dart
+++ lib/features/search/pesentation/views/widgets/search_list_view.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies_app/core/utils/widgets/custom_error_widget.dart';
+import 'package:movies_app/core/utils/widgets/custom_loading_indicator.dart';
+import 'package:movies_app/features/search/pesentation/manager/search_cubit/search_cubit.dart';
+import 'package:movies_app/features/search/pesentation/views/widgets/search_list_view_item.dart';
 
 class SearchListView extends StatelessWidget {
   const SearchListView({
@@ -7,6 +12,45 @@ class SearchListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    return BlocBuilder<SearchCubit, SearchState>(
+      builder: (context, state) {
+        if (state is SearchSuccess) {
+          return Expanded(
+            child: ListView.separated(
+              itemBuilder: (BuildContext context, index) {
+                return SearchListViewItem(
+                    movieResponse: state.movieModel.results![index],);
+              },
+              separatorBuilder: (BuildContext context, index) {
+                return Container(
+                  margin: const EdgeInsets.only(
+                      left: 2, right: 2, top: 18, bottom: 18),
+                  width: double.infinity,
+                  height: 1,
+                  color: const Color.fromRGBO(181, 180, 180, 1.0),
+                );
+              },
+              itemCount: state.movieModel.results!.length,
+            ),
+          );
+        } else if (state is SearchFailure) {
+          return CustomErrorWidget(errMessage: state.errMessage);
+        } else {
+          return const CustomLoadingIndicator();
+        }
+      },
+    );
   }
+
+  // List loadSearchData(context) {
+  //   BlocProvider.of<SearchCubit>(context).fetchSearch(query: searchKey).then((value){
+  //     result = value.results ?? [];
+  //   });
+  //   ApiRepository.fetchSearch(searchKey).then((value) {
+  //     result = value.results ?? [];
+  //   }).onError((error, stackTrace) {
+  //     print('error => ${error.toString()}');
+  //   });
+  //   return result;
+  // }
 }
---
+++ lib/features/search/pesentation/views/widgets/search_list_view_item.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/features/home/data/models/movie_model.dart';
+import 'package:movies_app/features/home/presentation/views/movie_details_view.dart';
+
+class SearchListViewItem extends StatelessWidget {
+  const SearchListViewItem(
+      {super.key, required this.movieResponse,});
+  final Results movieResponse;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      width: double.infinity,
+      height: 100,
+      child: Row(
+        children: [
+          Flexible(
+            child: Stack(
+              children: [
+                InkWell(
+                  onTap: () {
+                    Navigator.pushNamed(
+                      context,
+                      MovieDetailsView.routeName,
+                    );
+                  },
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(6),
+                    child: Image.network(
+                      'https://image.tmdb.org/t/p/w500'
+                          '${movieResponse.posterPath}',
+                      fit: BoxFit.cover,
+                      width: 150,
+                      height: 100,
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(4),
+                    child: Image.asset('assets/images/ic_bookmark.png'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(width: 10),
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  movieResponse.title ?? '',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(fontSize: 15, color: Colors.white),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  movieResponse.releaseDate ?? '',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    fontSize: 13,
+                    color: Color.fromRGBO(181, 180, 180, 1.0),
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  movieResponse.overview ?? '',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    fontSize: 13,
+                    color: Color.fromRGBO(181, 180, 180, 1.0),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
