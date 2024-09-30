f42f17e75d584e38656ab4e04e2bca34eebf167e
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-10-01 02:29:52 +0300

    provide and trigger GenreCubit in browse_view.dart

--- lib/features/browse/presentation/views/browse_view.dart
+++ lib/features/browse/presentation/views/browse_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies_app/core/utils/service_locator.dart';
+import 'package:movies_app/features/browse/data/repos/browse_repo_implementation.dart';
+import 'package:movies_app/features/browse/presentation/manager/genre_cubit/genre_cubit.dart';
 import 'package:movies_app/features/browse/presentation/views/widgets/browse_view_body.dart';
 
 class BrowseView extends StatelessWidget {
@@ -6,8 +10,13 @@ class BrowseView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: BrowseViewBody(),
+    return SafeArea(
+      child: BlocProvider(
+        create: (context) => GenreCubit(
+          getIt.get<BrowseRepoImplementation>(),
+        )..fetchGenre(),
+        child: const BrowseViewBody(),
+      ),
     );
   }
 }
