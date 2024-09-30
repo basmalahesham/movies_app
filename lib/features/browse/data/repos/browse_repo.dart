3b48153b930ce360682730d91a2d910424b63b03
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-10-01 01:20:44 +0300

    implement fetchGenre & fetchMoviesList functions

--- lib/features/browse/data/repos/browse_repo.dart
+++ lib/features/browse/data/repos/browse_repo.dart
@@ -1,8 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:movies_app/core/errors/failures.dart';
 import 'package:movies_app/features/browse/data/models/genre_model.dart';
+import 'package:movies_app/features/home/data/models/movie_model.dart';
 
 abstract class BrowseRepo {
   Future<Either<Failure, GenreModel>> fetchGenre();
-  Future<Either<Failure, GenreModel>> fetchMoviesList();
+  Future<Either<Failure, MovieModel>> fetchMoviesList();
 }
--- lib/features/browse/data/repos/browse_repo_implementation.dart
+++ lib/features/browse/data/repos/browse_repo_implementation.dart
@@ -1,19 +1,54 @@
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:movies_app/core/errors/failures.dart';
+import 'package:movies_app/core/utils/api_service.dart';
 import 'package:movies_app/features/browse/data/models/genre_model.dart';
 import 'package:movies_app/features/browse/data/repos/browse_repo.dart';
+import 'package:movies_app/features/home/data/models/movie_model.dart';
 
 class BrowseRepoImplementation implements BrowseRepo{
+  final ApiService apiService;
+
+  BrowseRepoImplementation(this.apiService);
   @override
-  Future<Either<Failure, GenreModel>> fetchGenre() {
-    // TODO: implement fetchGenre
-    throw UnimplementedError();
+  Future<Either<Failure, GenreModel>> fetchGenre() async{
+    try {
+      var data = await apiService.get(endpoint: 'genre/movie/list');
+      var result = GenreModel.fromJson(data);
+      return right(result);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioException(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
+
   }
 
   @override
-  Future<Either<Failure, GenreModel>> fetchMoviesList() {
-    // TODO: implement fetchMoviesList
-    throw UnimplementedError();
+  Future<Either<Failure, MovieModel>> fetchMoviesList() async{
+    try {
+      var data = await apiService.get(endpoint: 'discover/movie');
+      var result = MovieModel.fromJson(data);
+      return right(result);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioException(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
   }
 
 }
