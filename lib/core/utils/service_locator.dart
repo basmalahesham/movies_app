57d24e68d1a28e80f2bdd04bec4f2fba7f314e9c
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-10-01 02:27:26 +0300

    register BrowseRepoImplementation in service_locator.dart

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:movies_app/core/utils/api_service.dart';
+import 'package:movies_app/features/browse/data/repos/browse_repo_implementation.dart';
 import 'package:movies_app/features/home/data/repos/home_repo_implementation.dart';
 import 'package:movies_app/features/search/data/repos/search_repo_implementation.dart';
 
@@ -11,4 +12,6 @@ void setUpServiceLocator() {
       HomeRepoImplementation(getIt.get<ApiService>()));
   getIt.registerSingleton<SearchRepoImplementation>(
       SearchRepoImplementation(getIt.get<ApiService>()));
+  getIt.registerSingleton<BrowseRepoImplementation>(
+      BrowseRepoImplementation(getIt.get<ApiService>()));
 }
