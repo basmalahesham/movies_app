ca29bf3a96eef0bddcd2133b9aa5bd355a37a782
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-30 17:34:05 +0300

    provide SearchCubit in search_view.dart

--- lib/features/search/pesentation/views/search_view.dart
+++ lib/features/search/pesentation/views/search_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies_app/core/utils/service_locator.dart';
+import 'package:movies_app/features/search/data/repos/search_repo_implementation.dart';
+import 'package:movies_app/features/search/pesentation/manager/search_cubit/search_cubit.dart';
 import 'package:movies_app/features/search/pesentation/views/widgets/search_view_body.dart';
 
 class SearchView extends StatelessWidget {
@@ -6,8 +10,13 @@ class SearchView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: SearchViewBody(),
+    return SafeArea(
+      child: BlocProvider(
+        create: (context) => SearchCubit(
+          getIt.get<SearchRepoImplementation>(),
+        ),
+        child: const SearchViewBody(),
+      ),
     );
   }
 }
