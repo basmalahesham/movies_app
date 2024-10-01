d9a5029b83773cc36d2a0793790fd1b328c8dab5
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-10-01 03:43:26 +0300

    fix issue in api_service.dart

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -5,8 +5,10 @@ class ApiService {
   final _apiKey = '5eaf06d00921bf74711bebae7460e509';
   final Dio _dio;
   ApiService(this._dio);
-  Future<dynamic> get({required String endpoint, String? query,String?queryName}) async {
-    var response = await _dio.get('$_baseUrl$endpoint?api_key=$_apiKey&$queryName=$query');
+  Future<dynamic> get(
+      {required String endpoint, String? query, String? queryName}) async {
+    var response =
+        await _dio.get('$_baseUrl$endpoint?api_key=$_apiKey&$queryName=$query');
     return response.data;
   }
 }
