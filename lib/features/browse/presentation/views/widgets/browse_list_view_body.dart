2c2b6bb3cd9d30f27d5f4f595e041c2386622e53
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-10-01 20:01:32 +0300

    trigger MoviesListCubit in browse_list_view_body.dart & display the data

--- lib/features/browse/presentation/views/widgets/browse_list_view_body.dart
+++ lib/features/browse/presentation/views/widgets/browse_list_view_body.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies_app/features/browse/data/models/genre_model.dart';
+import 'package:movies_app/features/browse/presentation/manager/movies_list_cubit/movies_list_cubit.dart';
 import 'package:movies_app/features/browse/presentation/views/widgets/custom_browse_list_view_separated.dart';
 
 class BrowseListViewBody extends StatelessWidget {
@@ -6,15 +9,18 @@ class BrowseListViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var args = ModalRoute.of(context)!.settings.arguments as Genres;
+    BlocProvider.of<MoviesListCubit>(context)
+        .fetchMoviesList(categoryName: args.name??'');
     return Scaffold(
       appBar: AppBar(
         iconTheme: const IconThemeData(
           color: Colors.white,
         ),
         backgroundColor: const Color(0xFF1D1E1D),
-        title: const Text(
-          '{generResponse!.name} List',
-          style: TextStyle(
+        title:  Text(
+          '${args.name} List',
+          style: const TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.bold,
             color: Colors.white,
