4d2262cfeba4f3069f3f85ff6c5c92746b3ef728
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-10-02 17:30:40 +0300

    integrate watch_list_cubit.dart with watch_list_view.dart

--- lib/features/watch_list/pesentation/views/widgets/watch_list_view.dart
+++ lib/features/watch_list/pesentation/views/widgets/watch_list_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies_app/features/watch_list/pesentation/manager/watch_list_cubit/watch_list_cubit.dart';
 import 'package:movies_app/features/watch_list/pesentation/views/widgets/watch_list_view_item.dart';
 
 class WatchListView extends StatelessWidget {
@@ -7,21 +9,27 @@ class WatchListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: ListView.separated(
-        separatorBuilder: (buildContext, index) => Container(
-          margin: const EdgeInsets.only(
-            left: 2,
-            right: 2,
-            top: 18,
-            bottom: 18,
-          ),
-          width: double.infinity,
-          height: 1,
-          color: Colors.grey,
-        ),
-        itemCount: 10,
-        itemBuilder: (buildContext, index) {
-          return const WatchListViewItem();
+      child: BlocBuilder<WatchListCubit, WatchListState>(
+        builder: (context, state) {
+          return ListView.separated(
+            separatorBuilder: (buildContext, index) => Container(
+              margin: const EdgeInsets.only(
+                left: 2,
+                right: 2,
+                top: 18,
+                bottom: 18,
+              ),
+              width: double.infinity,
+              height: 1,
+              color: Colors.grey,
+            ),
+            itemCount: context.read<WatchListCubit>().watchList.length,
+            itemBuilder: (buildContext, index) {
+              return WatchListViewItem(
+                movieResult: context.read<WatchListCubit>().watchList[index],
+              );
+            },
+          );
         },
       ),
     );
