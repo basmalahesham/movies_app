da4b9c491cc09b2a0f0eba83b5a3c0c7d561ec80
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-10-02 17:29:40 +0300

    provide watch_list_cubit.dart with main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies_app/core/utils/app_styles.dart';
 import 'package:movies_app/core/utils/service_locator.dart';
 import 'package:movies_app/features/browse/presentation/views/browse_list_view.dart';
 import 'package:movies_app/features/home/presentation/views/movie_details_view.dart';
 import 'package:movies_app/features/layout/presentation/views/home_layout.dart';
 import 'package:movies_app/features/splash/presentation/views/splash_view.dart';
+import 'package:movies_app/features/watch_list/pesentation/manager/watch_list_cubit/watch_list_cubit.dart';
 
 
 void main() {
@@ -17,16 +19,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: MyThemeData.defaultTheme,
-      initialRoute: SplashView.routeName,
-      routes: {
-        SplashView.routeName: (context) => const SplashView(),
-        HomeLayout.routeName: (context) => const HomeLayout(),
-        MovieDetailsView.routeName: (context) => const MovieDetailsView(),
-        BrowseListView.routeName: (context) => const BrowseListView(),
-      },
+    return BlocProvider(
+      create: (context) => WatchListCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: MyThemeData.defaultTheme,
+        initialRoute: SplashView.routeName,
+        routes: {
+          SplashView.routeName: (context) => const SplashView(),
+          HomeLayout.routeName: (context) => const HomeLayout(),
+          MovieDetailsView.routeName: (context) => const MovieDetailsView(),
+          BrowseListView.routeName: (context) => const BrowseListView(),
+        },
+      ),
     );
   }
 }
